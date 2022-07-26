c58564d21b86eb6dd70d69a4b5f880545bfa7389
Rafael Bordoni <49042948+eldskald@users.noreply.github.com>
2022-07-26 19:14:42 -0300

    Add second answer

--- respostas.sql
+++ respostas.sql
@@ -1,2 +1,2 @@
 SELECT users.id, users.name, cities.name as cities FROM users JOIN cities ON cities.id = users."cityId"
-
+SELECT testimonials.id, writers.name as writer, recipients.name as recipient, testimonials.message FROM testimonials JOIN users writers ON writers.id = testimonials."writerId" JOIN users recipients ON recipients.id = testimonials."recipientId" ORDER BY testimonials.id;
